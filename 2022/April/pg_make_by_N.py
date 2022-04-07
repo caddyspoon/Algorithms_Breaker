c37878aa4d7f26e982a573b6c9f9e40b8d8bf17a
caddySpoon <cwb5116@gmail.com>
2022-04-07 21:08:29 +0900

    프로그래머스 | 동적계획법 > N으로 표현

--- 2022/April/pg_make_by_N.py
+++ 2022/April/pg_make_by_N.py
@@ -9,39 +9,41 @@ def solution(N, number):
         clist = dp[crntNum] = []
 
         if int(str(N)*crntNum) == number:
-            return number
+            return crntNum
 
         clist.append(int(str(N)*crntNum))
 
         for i in range(1, crntNum):
             pair_num = crntNum - i
 
-            if i > pair_num: break
-
             for elm1 in dp[i]:
                 for elm2 in dp[pair_num]:
+
                     if elm1+elm2 not in clist:
                         if elm1+elm2 == number:
                             return crntNum
                         clist.append(elm1+elm2)
 
-                    if abs(elm1-elm2) not in clist:
-                        if abs(elm1-elm2) == number:
+                    if elm1-elm2 not in clist:
+                        if elm1-elm2 == number:
                             return crntNum
-                        clist.append(abs(elm1-elm2))
+                        clist.append(elm1-elm2)
 
                     if elm1*elm2 not in clist:
                         if elm1*elm2 == number:
                             return crntNum
                         clist.append(elm1*elm2)
 
-                    if elm1 > elm2:
-                        elm1, elm2 = elm2, elm1
-                    if elm1 != 0:
-                        if elm2//elm1 == number:
-                            return crntNum
-
-                        if elm2 // elm1 not in clist:
-                            clist.append(elm2//elm1)
+                    if elm2 != 0:
+                        if elm1 // elm2 not in clist:
+                            if elm1 // elm2 == number:
+                                return crntNum
+                            clist.append(elm1//elm2)
         crntNum += 1
     return answer
+
+N = 5
+number = 12
+result = 4
+
+print(solution(N, number))
