e341aa36a1adff015662ed9e3935a2009f802278
caddySpoon <cwb5116@gmail.com>
2022-05-27 00:59:08 +0900

    프로그래머스 | 카카오 2020 > 가사검색 (해결)

--- 2022/May/pg_searching_lyrics(solving).js
+++ 2022/May/pg_searching_lyrics(solving).js
@@ -1,77 +1,83 @@
 class Node {
     constructor(key, data = null) {
-        this.key = key
-        this.data = data
-        this.children = {}
+        this.key = key;
+        this.data = data;
+        this.children = {};
+        this.cnt = 0;
     }
 }
 
 class Trie {
     constructor() {
-        this.head = new Node(null)
+        this.head = new Node(null);
     }
 
     insert(string) {
-        let currNode = this.head
+        let currNode = this.head;
 
-        for (const char of string) {
+        for (let i = 0; i < string.length; i++) {
+            const char = string[i];
             if (!(char in currNode.children)) {
-                currNode.children[char] = new Node(char)
+                currNode.children[char] = new Node(char);
             }
 
-            currNode = currNode.children[char]
+            currNode.cnt += 1;
+            currNode = currNode.children[char];
         }
 
-        currNode.data = string
+        // currNode.data = string
     }
 
     search(string) {
-        let currNode = this.head
+        let currNode = this.head;
 
-        for (const char of string) {
+        for (let i = 0; i < string.length; i++) {
+            const char = string[i];
             if (char in currNode.children) {
-                currNode = currNode.children[char]
-            } else if (char === '?') {
-                for (const elm in currNode.children) {
-                    currNode = currNode.children[elm]
-                    // FIXME
-                }
+                currNode = currNode.children[char];
+            } else if (char === "?") {
+                return currNode.cnt
             } else {
-                return false
+                return 0;
             }
         }
 
-        if (currNode.data !== null) {
-            return true
-        } else {
-            return false
-        }
     }
 }
 
 function solution(words, queries) {
-    const answer = []
+    const answer = [];
 
-    // for (const word of words) {
-    //     trie.insert(word)
-    // }
+    const trieObj = {};
 
 
-    for (const query of queries) {
-        let answerCnt = 0
-
-        for (const word of words) {
-            if (word.length !== query.length) {
-                continue
-            }
-            const trie = new Trie()
-            trie.insert(word)
-            if (trie.search(query)) {
-                answerCnt += 1
-            }
+    for (const word of words) {
+        const len = word.length;
+        if (!(len in trieObj)) {
+            trieObj[len] = [new Trie(), new Trie()]
         }
-        answer.push(answerCnt)
+        trieObj[len][0].insert(word);
+        trieObj[len][1].insert(word.split("").reverse().join(""));
     }
 
-    return answer
+    for (let query of queries) {
+        try{
+            if (query[0] === "?") {
+                query = query.split("").reverse().join("");
+                answer.push(trieObj[query.length][1].search(query));
+            } else {
+                answer.push(trieObj[query.length][0].search(query));
+            }
+        } catch {
+            answer.push(0)
+        }
+    }
+
+    return answer;
 }
+
+const words = ["frodo", "front", "frost", "frozen", "frame", "kakao"];
+const queries = ["fro??", "????o", "fr???", "fro???", "pro?"];
+const answer = [3, 2, 4, 1, 0];
+
+console.log(solution(words, queries) === answer);
