38fee74ee6eafb53fa2bdd96047667fea574a5bd
caddySpoon <cwb5116@gmail.com>
2022-06-02 22:38:08 +0900

    카카오 2018 | 추석 트래픽 (일부 수정)

--- 2022/June/pg_kakao2018_chuseok_traffic.js
+++ 2022/June/pg_kakao2018_chuseok_traffic.js
@@ -8,7 +8,7 @@ function solution(lines) {
     const spendToms = Number(line.split(" ")[2].slice(0, -1)) * 1000;
 
     times.push([new Date(timeStamp - spendToms + 1), timeStamp]);
-  });
+  })
     
   for (let i = 0; i < times.length; i++) {
     let cnt = 0;
@@ -16,7 +16,7 @@ function solution(lines) {
     let startTime = times[i][1];
 
     let endTime = new Date(times[i][1].getTime());
-    endTime = new Date(endTime.setSeconds(endTime.getSeconds() + 1));  
+    endTime = endTime.setSeconds(endTime.getSeconds() + 1);  
 
     for (let j = i; j < times.length; j++) {
       if (times[j][0] < endTime && times[j][1] >= startTime) {
