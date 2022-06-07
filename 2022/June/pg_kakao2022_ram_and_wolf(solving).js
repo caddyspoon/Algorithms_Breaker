84116dd4c8475970bd551313cf166abc11e0c530
caddySpoon <cwb5116@gmail.com>
2022-06-07 23:34:23 +0900

    프로그래멋 | 카카오 2022 양과늑대

--- 2022/June/pg_kakao2022_ram_and_wolf(solving).js
+++ 2022/June/pg_kakao2022_ram_and_wolf(solving).js
@@ -7,39 +7,44 @@ function solution(info, edges) {
   edges.forEach(node => {
     tree[node[0]] = [ ...tree[node[0]], node[1] ];
     tree[node[1]] = [ ...tree[node[1]], node[0] ];
-  })
+  });
 
   const initInfo = [ ...info ];
 
   const recur = (cur = 0, sheep = 0, wolf = 0, cnt = 1, info = initInfo, visited = intVisited) => {
-    if (cnt === info.length) {
+    if (cnt > info.length) {
       if (sheep > answer) {
         answer = sheep;
       }
       return;
     }
 
-    if (info[cur] === 0) {
-      sheep += 1;
-    } else if (info[cur] === 1) {
-      wolf += 1;
-      if (wolf >= sheep) {
-        if (sheep > answer) {
-          answer = sheep;
+    const tempInfo = [ ...info ];
+    let tempVisited = [ ...visited ];
+
+    if (typeof tempInfo[cur] === 'number') {
+      if (tempInfo[cur] === 0) {
+        sheep += 1;
+      } else if (tempInfo[cur] === 1) {
+        wolf += 1;
+        if (wolf === sheep) {
+          if (sheep > answer) {
+            answer = sheep;
+          }
+          return;
         }
-        return;
       }
+      cnt += 1;
+      tempInfo[cur] = false;
+      tempVisited = new Array(info.length).fill(false); 
     }
 
     tree[cur].forEach(elm => {
-      console.log("cur: ", cur)
-      console.log("elm: ", elm)
-      console.log()
-      if (!visited[elm]) {
-        visited[elm] = true;
-        recur(elm, sheep, wolf, cnt + 1, visited);
+      if (!tempVisited[elm]) {
+        tempVisited[elm] = true;
+        recur(elm, sheep, wolf, cnt, tempInfo, tempVisited);
 
-        visited[elm] = false;
+        tempVisited[elm] = false;
       }
     });
   };
@@ -47,96 +52,8 @@ function solution(info, edges) {
   intVisited[0] = true;
   recur();
 
-  console.log(answer)
+  return answer;
 }
-
-
-// function solution(info, edges) {
-//   let answer = 0;
-
-//   const tree = new Array(info.length).fill(new Array());
-//   let visited = new Array(info.length).fill(false);
-
-//   edges.forEach(node => {
-//     tree[node[0]] = [ ...tree[node[0]], node[1] ];
-//     tree[node[1]] = [ ...tree[node[1]], node[0] ];
-//   })
-
-//   // const recur = () => [
-    
-//   // ]
-
-
-//   const currInfo = {
-//     loc: 0,
-//     sheep: 0,
-//     wolf: 0,
-//   }
-
-//   visited[0] = true;
-//   const stack = [0];
-
-//   while (stack.length) {
-//     currNode = stack.pop();
-
-//     currInfo.loc = currNode;
-//     if (info[currNode] === 0) {
-//       currInfo.sheep += 1;
-//       info[currNode] = false;
-//       visited = new Array(info.length).fill(false);
-//     } else if (info[currNode] === 1) {
-//       currInfo.wolf += 1;
-//       info[currNode] = false;
-//       visited = new Array(info.length).fill(false);
-//     }
-
-//     visited[currNode] = true;
-    
-//     console.log("currInfo: ", currInfo);
-//     // console.log(tree);
-//     console.log("info: ", info);
-//     console.log("visited: ", visited);
-//     console.log();
-//     console.log();
-//     console.log();
-
-//     // 자식들을 꺼냄
-//     tree[currNode].forEach(elm => {
-//       // 방문하지 않은 곳이며
-//       if (!visited[elm]) {
-//         const animalInfo = info[elm];
-
-//         // 해당 칸에 아직 동물이 있을 때
-//         if (typeof animalInfo === "number") {
-          
-//           // 양일 때
-//           if (animalInfo === 0) {
-//             stack.push(elm);
-//             visited[elm] = true;
-//           // 늑대일 때
-//           } else if (animalInfo === 1) {
-//             // 갈 수 있으면 간다
-//             if (currInfo.sheep <= currInfo.wolf + 1) {
-//               return;
-//             } else {
-//               stack.push(elm);
-//               visited[elm] = true;
-//             }
-//           }
-
-//           // 이미 데려온 칸일 때
-//         } else {
-//           stack.push(elm);
-//           visited[elm] = true;
-//         }
-//       }
-//     })
-//   }
-
-//   answer = currInfo.sheep;
-//   console.log(answer)
-//   return answer;
-// }
 
 // const info = [0,0,1,1,1,0,1,0,1,0,1,1];
 // const edges = [[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]];
