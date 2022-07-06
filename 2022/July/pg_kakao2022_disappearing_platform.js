1f9e14191cec32388a46d5c35b583b2641f04c38
caddySpoon <cwb5116@gmail.com>
2022-07-06 23:05:13 +0900

    0706

---
+++ 2022/July/pg_kakao2022_disappearing_platform.js
@@ -0,0 +1,33 @@
+function solution(board, aloc, bloc) {
+    let answer = -1;
+
+    console.log(board);
+
+    return answer;
+}
+
+// Test Case 1.
+const board = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+const aloc = [1, 0];
+const bloc = [1, 2];
+const answer = 5;
+
+// // Test Case 2.
+// const board = [[1, 1, 1], [1, 0, 1], [1, 1, 1]];
+// const aloc = [1, 0];
+// const bloc = [1, 2];
+// const answer = 4;
+
+// Test Case 3.
+// const board = [[1, 1, 1, 1, 1]];
+// const aloc = [0, 0];
+// const bloc = [0, 4];
+// const answer = 4;
+//
+// // Test Case 4.
+// const board = [[1]];
+// const aloc = [0, 0];
+// const bloc = [0, 0];
+// const answer = 0;
+
+console.log(solution(board, aloc, bloc) === answer);
