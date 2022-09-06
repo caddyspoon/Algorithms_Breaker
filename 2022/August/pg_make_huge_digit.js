c8b1fdc0e84903c0fd9791da7f90d50ccd6af977
caddySpoon <cwb5116@gmail.com>
2022-09-07 01:49:36 +0900

    프로그래머스 | 큰 수 만들기

--- 2022/August/pg_make_huge_digit.js
+++ 2022/August/pg_make_huge_digit.js
@@ -1,33 +1,29 @@
-const solution = (number, k) => {
-
-  let changed = true;
-  let modiNum = [...number]
-
-  while (k > 0 && changed) {
-    changed = false;
-    let acumIdx = 1;
+const solution = (number, k) => {   
+  let stack = [];
   
-    for (let i = 1; i < modiNum.length; i++) {
-      const prevNo = modiNum[i-1];
-      const curNo = modiNum[i];
-  
-      if (Number(prevNo) < Number(curNo)) {
-        // 잘라주는 뭔가를 한다.
-        modiNum = [...modiNum.slice(acumIdx)]
-        console.log("I cut it! ", modiNum)
-        changed = true;
+  for (num of number) {    
+    if (stack.at(-1) === 9) {
+      stack.push(num);
+      continue;
+    }
+      
+    if (stack.at(-1) < num) {
+      while (k > 0 && stack.at(-1) < num && stack.length) {
+        stack.pop();
+        k -= 1;
       }
-  
-      acumIdx += 1;
-      if (acumIdx > k) {
-        break;
-      }
-      // 그렇지 않으면 누적 합계를 증가, k보다 작을 때까지만
-    };
+    }
+
+    stack.push(num);
+  };
+
+  if (k > 0) {
+    stack = stack.slice(0, stack.length - k)
   }
 
-  
-};
+  return stack.join("");
+}
+
 
 const number = "4177252841";
 const k =	4;	
